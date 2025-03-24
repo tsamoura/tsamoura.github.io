681b669646080fc2ef7c74c608ffbb0e2611ce39
tsamoura <efthymia.tsamoura@gmail.com>
2025-03-24 21:06:55 +0000

    Deploying to gh-pages from @ tsamoura/tsamoura.github.io@9d0cf4cc14ce08ad0e99a031bed72f451a046401 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,9 +440,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-pdq",
+          title: 'PDQ',
+          description: "Proof-Driven Querying",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
