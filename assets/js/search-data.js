76f332c6d2fe12b298436fca1f0c62034ff7bf76
tsamoura <efthymia.tsamoura@gmail.com>
2025-03-24 18:53:45 +0000

    Deploying to gh-pages from @ tsamoura/tsamoura.github.io@1e55b5e193bab1492f20d098b21a9ad7e5dd5e19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
