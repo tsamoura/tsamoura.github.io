b8ef925c9a140679c7af0eaf43dce2dc643adc98
tsamoura <efthymia.tsamoura@gmail.com>
2025-03-24 21:34:53 +0000

    Deploying to gh-pages from @ tsamoura/tsamoura.github.io@dbf0125a5eb86a84f831fff6040422cf59c2d148 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,46 +445,26 @@ ninja.data = [{
           description: "Proof-Driven Querying",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-chasebench",
+          title: 'ChaseBench',
+          description: "Benchmarking The Chase",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-chasegoal",
+          title: 'ChaseGoal',
+          description: "Query Answering Over Knowledge Graphs",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-vproblog",
+          title: 'vProbLog',
+          description: "Probabilistic Query Answering using Provenance Semirings",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-neurolog",
+          title: 'NeuroLog',
+          description: "Probabilistic Query Answering using Provenance Semirings",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
